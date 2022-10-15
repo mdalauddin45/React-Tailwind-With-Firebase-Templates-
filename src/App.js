57742ba73c77eb099f00c7c80859b977eb703d18
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-10-15 10:39:05 +0600

    sfdgsdf

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
+import ErrorPage from "./components/Error/ErrorPage";
+import Home from "./components/Home/Home";
 import Main from "./components/layout/Main";
 
 function App() {
@@ -7,6 +9,11 @@ function App() {
     {
       path: "/",
       element: <Main></Main>,
+      errorElement: <ErrorPage></ErrorPage>,
+      children: [
+        { path: "/", element: <Home></Home> },
+        { path: "/home", element: <Home></Home> },
+      ],
     },
   ]);
   return (
